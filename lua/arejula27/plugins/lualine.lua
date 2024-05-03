18b33238f27d2b71e6cbf3b9c9e270fa02baf859
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-05-03 23:32:12 +0200

    add lualine

---
+++ lua/arejula27/plugins/lualine.lua
@@ -0,0 +1,37 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+  config = function()
+    require('lualine').setup {
+      options = {
+        --    theme = "", -- check: https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = {
+          { 'mode', separator = { left = '' }, right_padding = 2 },
+        },
+        lualine_b = { 'filename', 'branch' },
+        lualine_c = { 'fileformat' },
+        lualine_x = { 'diagnostics' },
+        lualine_y = { 'filetype', 'progress' },
+        lualine_z = {
+          { 'location', separator = { right = '' }, left_padding = 2 },
+        },
+      },
+      inactive_sections = {
+        lualine_a = { 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
+      },
+      tabline = {
+        --lualine_c={'buffers'},
+        lualine_z = { { 'tabs', separator = { right = '', left = '' }, left_padding = 2 } },
+      },
+    }
+  end,
+}
