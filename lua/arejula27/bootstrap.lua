2cc53524033989645beaff041268ea3ab625872a
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-05-04 16:47:11 +0200

    improve boostrap, detect gut folders

--- lua/arejula27/bootstrap.lua
+++ lua/arejula27/bootstrap.lua
@@ -1,3 +1,8 @@
+-- bootstrap file this while  initiate vim
+local is_git_dir = function()
+  return os.execute 'git rev-parse --is-inside-work-tree >> /dev/null 2>&1'
+end
+
 --This function is binded ti VimEnter, which is call when vim loads.
 vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
@@ -5,7 +10,11 @@ vim.api.nvim_create_autocmd('VimEnter', {
     if vim.fn.isdirectory(bufferPath) ~= 0 then
       local ts_builtin = require 'telescope.builtin'
       vim.api.nvim_buf_delete(0, { force = true })
-      ts_builtin.find_files()
+      if is_git_dir() == true then
+        ts_builtin.git_files { show_untracked = true }
+      else
+        ts_builtin.find_files()
+      end
     end
   end,
 })
