4be60536ce0e468af89fea308db2a993a9440bc8
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-05-05 17:13:26 +0200

    allow obsidian follow external links (browser)

--- lua/arejula27/plugins/obsidian.lua
+++ lua/arejula27/plugins/obsidian.lua
@@ -92,6 +92,16 @@ return {
         end
         return 'Note-' .. suffix
       end,
+
+      -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+      -- URL it will be ignored but you can customize this behavior here.
+      ---@param url string
+      follow_url_func = function(url)
+        -- Open the URL in the default web browser.
+        --  vim.fn.jobstart { 'open', url } -- Mac OS
+        vim.fn.jobstart { 'xdg-open', url } -- linux
+      end,
+
       -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
       -- way then set 'mappings = {}'.
       mappings = {
