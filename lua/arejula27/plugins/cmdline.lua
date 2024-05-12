4e7fc5c2538dcf90c3c6719b6c125fade1550946
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-05-12 21:25:27 +0200

    disable cmdline plugin

--- lua/arejula27/plugins/cmdline.lua
+++ lua/arejula27/plugins/cmdline.lua
@@ -1,8 +1,9 @@
 return {
 
   'VonHeikemen/fine-cmdline.nvim',
+  enabled = false,
   dependencies = {
     { 'MunifTanjim/nui.nvim' },
   },
-  vim.api.nvim_set_keymap('n', '<CR>', '<cmd>FineCmdline<CR>', { noremap = true }),
+  --  vim.api.nvim_set_keymap('n', '<CR>', '<cmd>FineCmdline<CR>', { noremap = true }),
 }
